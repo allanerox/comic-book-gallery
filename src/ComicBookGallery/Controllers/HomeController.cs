72babc534146858437e7f5f59d53eaa7e73be830
Allan Fett <allan.fett@hotmail.com>
2017-06-15 11:15:25 +0930

    Added Home Controller

---
+++ src/ComicBookGallery/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        { 
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return new RedirectResult("/");
+            }
+            return new ContentResult()
+            {
+                Content = "Hello from the Home Controller"
+            };
+        }
+    }
+}
