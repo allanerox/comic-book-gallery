51a92f692adb7fd94d2991c1bbe122df09d60ac1
Allan Fett <allan.fett@hotmail.com>
2017-06-17 01:02:51 +0930

    Added Comic Books List Views

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,12 +10,18 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-
         private ComicBookRepository _comicBookRepository = null;
 
         public ComicBooksController()
         {
             _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetComicBooks();
+
+            return View(comicBooks);
         }
 
         public ActionResult Detail(int? id)
